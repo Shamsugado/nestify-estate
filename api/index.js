a58a43643b1f251473a60fc51dc3d1772974c2a7
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2026-01-06 14:34:39 -0500

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_PASSWORD).then(() => {
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_PASSWORD).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('API server is running on port 3000');
 });
@@ -19,3 +22,4 @@ app.listen(3000, () => {
 // app.use('/api/users', require('./routes/user.route.js').default);
 // The above line is replaced with the following to use ES6 module import. As we have already imported userRouter.
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
