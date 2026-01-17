ecd75a68e055c56731e6beccab50b6eb560ed1ea
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2026-01-17 15:35:29 -0500

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,82 @@
-import React from 'react'
+import { useEffect, useState} from 'react'
 import { useSelector } from 'react-redux'
+import { useRef} from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from '../firebase';
 
 const Profile = () => {
   const currentUser = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePercent, setFilePercent] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  // console.log(formData);
+  // console.log(filePercent);
+  // console.log(fileUploadError);
+
+
+   // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+   useEffect(() => {
+    if (file) {
+      // upload file to firebase storage
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + '-' + file.name;
+    const storageRef = ref(storage, fileName);
+    // Upload the file and metadata
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePercent(Math.round(progress));
+        // console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        setFileUploadError(true);
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({...formData, photoURL: downloadURL});
+        });
+      }
+    )
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        {/* <img src={currentUser.avatar} alt="" /> */}
-        <img 
-                src={currentUser.photoURL} 
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+        <img onClick={() => fileRef.current.click()}
+                src={formData.photoURL || currentUser.photoURL} 
                 alt="User Avatar" 
                 className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"
               />
+              <p className='text-center mt-2'>
+                {fileUploadError?
+                  <span className='text-red-700'>Error uploading image. (Image must be less than 2 MB).</span> :
+                  filePercent > 0 && filePercent < 100 ?
+                  <span className='text-slate-700'>{`Uploading ${filePercent}%`}</span> :
+                  filePercent === 100 ? <span className='text-green-700'>Image uploaded successfully!</span> 
+                  : ""
+                }
+              </p>
   <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
   <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'/>
   <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg'/>
@@ -32,5 +96,6 @@ const Profile = () => {
     </div>
   )
 }
+
 
 export default Profile
