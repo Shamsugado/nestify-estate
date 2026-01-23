0deb68117d6c7a1d33f4165b5445afbec2fcd9bf
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2026-01-23 07:49:24 -0500

    add sign out user functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import e from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
+import { google, signin, signOut, signup } from "../controllers/auth.controller.js";
+
 
 const router = e.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signOut);
 export default router;
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -47,9 +47,20 @@ export const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 export const { signInStart, signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure,
-  deleteUserSuccess, deleteUserStart, } = userSlice.actions;
-
+  deleteUserSuccess, deleteUserStart, signOutUserStart, signOutUserSuccess, signOutUserFailure } = userSlice.actions;
 export default userSlice.reducer;
