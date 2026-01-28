a6ca7f130285c28c75fed4ab5dce56de8bdd995a
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2026-01-28 07:41:25 -0500

    create get user listings api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import Listing from './models/listing.model.js';
+import listingRouter from './routes/listing.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_PASSWORD).then(() => {
@@ -27,7 +27,7 @@ app.listen(3000, () => {
 // The above line is replaced with the following to use ES6 module import. As we have already imported userRouter.
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/listing', Listing);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
