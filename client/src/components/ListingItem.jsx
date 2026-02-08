f9408b166ed8bf9b28220958b5628c09e6136dd7
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2026-02-08 14:23:32 -0500

    add pre installation

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -2,7 +2,7 @@
 import { Link } from 'react-router-dom';
 import { MdLocationOn } from 'react-icons/md';
 
-const ListingItem = () => {
+const ListingItem = ({ listing }) => {
   return (
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
       <Link to={`/listing/${listing._id}`}>
